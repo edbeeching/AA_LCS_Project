0e86fcd391c73933642ca517e185c202a80e9a6c
Ed Beeching <edbeeching@users.noreply.github.com>
2016-10-19 16:26:11 +0200

    The beginings of printing neatly

---
+++ code_testing/zeb_printing_neatly1.py
@@ -0,0 +1,16 @@
+
+
+
+
+def PrintNeatly(ListOfStrings, NumberOfLinesPerRow):
+
+    ## Function to print neatly
+
+    print"----------------"
+
+    print"----------------"
+
+if __name__ == '__main__':
+    ## run this following if this is the main program
+    testString = ["This", "is", "a", "test", "string", "for", "printing", "neatly"]
+    PrintNeatly(testString, 12)
