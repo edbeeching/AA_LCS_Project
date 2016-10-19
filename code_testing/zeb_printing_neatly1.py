64035c0a72fcf1a5627dc246aedc53bb6ba45b50
Ed Beeching <edbeeching@users.noreply.github.com>
2016-10-19 16:53:00 +0200

    The beginings of printing neatly

--- code_testing/zeb_printing_neatly1.py
+++ code_testing/zeb_printing_neatly1.py
@@ -1,16 +1,28 @@
 
+from __future__ import print_function
+import numpy
 
+def print_neatly(ListOfStrings, NumberOfLinesPerRow):
 
+    ## Function to print neatly using most basic way
 
-def PrintNeatly(ListOfStrings, NumberOfLinesPerRow):
+    line_counter = 0
+    print("----------------")
+    for string in ListOfStrings:
+        line_counter += len(string) + 1
+        if(line_counter > NumberOfLinesPerRow):
+            print()
+            line_counter = len(string) + 1
 
-    ## Function to print neatly
+        print(string,end=" ")
+    print("\n----------------")
 
-    print"----------------"
+    ## Computation of extras[i,j]
+    n = len(ListOfStrings)
+    extras = numpy.empty((n, n), dtype="int")
 
-    print"----------------"
 
 if __name__ == '__main__':
     ## run this following if this is the main program
     testString = ["This", "is", "a", "test", "string", "for", "printing", "neatly"]
-    PrintNeatly(testString, 12)
+    print_neatly(testString, 16)
