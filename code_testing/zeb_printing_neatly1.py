65b566d9044a1572be47899fe8e297a38848856a
Ed Beeching <edbeeching@users.noreply.github.com>
2016-10-20 17:19:43 +0200

    Printing neatly complete.

--- code_testing/zeb_printing_neatly1.py
+++ code_testing/zeb_printing_neatly1.py
@@ -6,30 +6,25 @@ import sys
 def compute_extras(i,j,listOfStrings, numberOfLinesPerRow):
 
     sum = numberOfLinesPerRow
-
     for string in listOfStrings[i:j+1]:
         sum -=len(string)
     sum-= (j-i)
-
-    ##print(listOfStrings[i:j + 1]," = ", sum)
-
     return sum
 
 def compute_line_cost(i, j, extras,n):
+
     if extras[i,j]<0:
-        ##return sys.maxint
-        return 10000
+        ##return sys.maxint This overflows in some cases, so better to use something smaller
+        return 10000000
     if j+1 is n:
         return 0
     return extras[i,j]**3
-
 
 def compute_cost(j,costs,line_cost):
     if j is 0:
         return line_cost[0,0], 0
 
-
-    sum = 10000
+    sum = 10000000
     k = 0
     for i in range(0,j+1):
         if i is 0:
@@ -50,11 +45,13 @@ def print_neatly(listOfStrings, numberOfLinesPerRow):
     print("----------------")
     for string in listOfStrings:
         line_counter += len(string) + 1
-        if(line_counter > numberOfLinesPerRow):
+        if(line_counter > numberOfLinesPerRow+1):
             print()
             line_counter = len(string) + 1
         print(string,end=" ")
     print("\n----------------")
+
+    ## The code below does printing neatly as per the definition in class
 
     ## Computation of extras[i,j]
     n = len(listOfStrings)
@@ -62,39 +59,35 @@ def print_neatly(listOfStrings, numberOfLinesPerRow):
     for i in range(0,n):
         for j in range(i,min(n, i + numberOfLinesPerRow/2)):
             extras[i,j] = compute_extras(i, j, listOfStrings, numberOfLinesPerRow)
-    ##print(extras)
+
     ## Computation of line costs
     line_cost = numpy.zeros((n, n), dtype="int32")
-    line_cost[:] = 100000
+    line_cost[:] = 10000000
     for i in range(0, n):
         for j in range(i, min(n, i + numberOfLinesPerRow / 2)):
             line_cost[i, j] = compute_line_cost(i, j, extras, n)
-    ##print(line_cost)
+
+    ## Computation of best costs
     costs = numpy.zeros(n, dtype="int32")
-    ##print(costs)
     para = numpy.zeros(n, dtype="int32")
     for j in range(0,n):
         costs[j], para[j] = compute_cost(j, costs, line_cost)
 
-    ##print(costs)
-    ##print(para)
-
+    ## The word key contains the a map of which strings are on which row
     word_key = [len(listOfStrings)]
     val = para[n-1]
-    word_key.insert(0,val)
-    ##print(val)
-    while(val>0):
+    word_key.insert(0, val)
+
+    while val > 0:
         val = para[val-1]
         word_key.insert(0, val)
 
-    ##print(word_key)
-
     print("----------------")
     key_index = 0
-    for i in range(0,len(listOfStrings)):
-        if word_key[key_index+1]<=i:
+    for i in range(0, len(listOfStrings)):
+        if word_key[key_index+1] <= i:
             print()
-            key_index+=1
+            key_index += 1
         print(listOfStrings[i], end=" ")
     print("\n----------------")
 
@@ -102,5 +95,12 @@ def print_neatly(listOfStrings, numberOfLinesPerRow):
 
 if __name__ == '__main__':
     ## run the following if this is the main program
+    ## There are a few test strings that can be compared.
     testString = ["This", "is", "a", "test", "string", "for", "printing", "neatly"]
-    print_neatly(testString, 16)
+    testString2 = ["Mickey", "rat", "likes", "to", "code", "everyday"]
+    test_file = open('../corpus-20090418/orig_taska.txt')
+    testString3 = []
+    for word in test_file.read().split():
+        testString3.append(word)
+
+    print_neatly(testString3, 32)
