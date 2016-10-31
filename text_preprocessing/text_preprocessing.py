4dbc9d46906d9f0ae3b00cf3c01dd52870d64419
Drydge <antho.deveaux@gmail.com>
2016-10-31 14:54:24 +0100

    Preproccessing
    replacing or modifying some char in a corpus fil and saving in an other
    foler called corpus-preprocessed

---
+++ text_preprocessing/text_preprocessing.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+import os
+def preproceesing_file(file):
+    file = file.replace('‘', '\'')
+    file = file.replace('’', '\'')
+    file = file.replace('“', '\"')
+    file = file.replace('”', '\"')
+    file = file.replace('\n\n', '\n')
+    file = file.replace('  ', ' ')
+    #file = file.replace('\'', '')
+    #file = file.replace('\"', '')
+    #file = file.replace('.', '')
+    #file = file.replace(',', '')
+    #file = file.replace('/', '')
+    #file = file.replace('?', '')
+    #file = file.replace('!', '')
+    #file = file.replace(';', '')
+
+    preprocessed_file =file
+    return preprocessed_file
+
+def read_file(path):
+    file_object = open(path)
+    file_string = file_object.read()
+    file_object.close()
+    return file_string
+
+def write_file(file, path):
+    file_object = open(path, 'w')
+    file_object.write(file)
+    file_object.close()
+    return
+
+def wrinting_preprocessed_file(filepath):
+    filepath = os.path.join(filepath)
+    file = read_file(filepath)
+    pathsep = filepath.split(os.path.sep)
+    pathsep[len(pathsep)-2] = 'corpus-preprocessed'
+    filepath_preprocessed = os.path.join('\\'.join(pathsep))
+    filepath_preprocessed += '_preprocessed'
+    write_file(preproceesing_file(file), filepath_preprocessed)
+    return
+
+file_list = os.listdir('..\\corpus-20090418')
+
+for each in file_list:
+    wrinting_preprocessed_file('..\\corpus-20090418\\'+each)
+
+
