4dddde1d705a4a74e6726b3cce591008c088f80d
Drydge <antho.deveaux@gmail.com>
2016-11-15 06:36:19 +0100

    Adv preprocessed SWR
    add features to advanced preproccessing
    Stop Word Removal

--- text_preprocessing/text_preprocessing.py
+++ text_preprocessing/text_preprocessing.py
@@ -2,7 +2,17 @@
 import os
 import platform
 import re
+def read_file(path):
+    file_object = open(path)
+    file_string = file_object.read()
+    file_object.close()
+    return file_string
 
+def write_file(file, path):
+    file_object = open(path, 'w')
+    file_object.write(file)
+    file_object.close()
+    return
 def preproceesing_file(file):
     file = file.replace('‘', '\'')
     file = file.replace('’', '\'')
@@ -29,7 +39,7 @@ def adv_preproceesing_file(file):
     # removing all non-ascii characters:
     file = re.sub(r'[^\x00-\x7F]+',' ', file)
 
-
+    file = file.lower()
     file = file.replace('‘', '\'')
     file = file.replace('’', '\'')
     file = file.replace('“', '\"')
@@ -47,34 +57,44 @@ def adv_preproceesing_file(file):
     file = file.replace(';', '')
     file = file.replace('(', '')
     file = file.replace(')', '')
+    StopWords = read_file("./LongListStopWords.txt").split()
+    for stopWord in StopWords:
+
+        file = file.replace(stopWord+' ', ' ')
+        file = file.replace(' '+stopWord+',', ',')
+        file = file.replace(' '+stopWord+'.', '.')
+        file = file.replace(' '+stopWord+' ', ' ')
 
     preprocessed_file =file
     return preprocessed_file
 
-def read_file(path):
-    file_object = open(path)
-    file_string = file_object.read()
-    file_object.close()
-    return file_string
 
-def write_file(file, path):
-    file_object = open(path, 'w')
-    file_object.write(file)
-    file_object.close()
-    return
 
 def wrinting_preprocessed_file(filepath):
     filepath = os.path.join(filepath)
     file = read_file(filepath)
     pathsep = filepath.split(os.path.sep)
+    pathsep[len(pathsep)-2] = 'corpus-preprocessed'
+    pathsep[len(pathsep)-1] = pathsep[len(pathsep)-1].replace('.txt', '_preprocessed.txt')
+    filepath_preprocessed = os.path.join('/'.join(pathsep))
+    write_file(preproceesing_file(file), filepath_preprocessed)
+    return
+
+def wrinting_adv_preprocessed_file(filepath):
+    filepath = os.path.join(filepath)
+    file = read_file(filepath)
+    pathsep = filepath.split(os.path.sep)
     pathsep[len(pathsep)-2] = 'corpus-adv_preprocessed'
-    pathsep[len(pathsep)-1] = pathsep[len(pathsep)-1].replace('.txt','_adv_preprocessed.txt')
+    pathsep[len(pathsep)-1] = pathsep[len(pathsep)-1].replace('.txt', '_adv_preprocessed.txt')
     filepath_preprocessed = os.path.join('/'.join(pathsep))
     write_file(adv_preproceesing_file(file), filepath_preprocessed)
     return
 
 
-
-file_list = os.listdir('../corpus-20090418')
+sep=os.path.sep
+file_list = os.listdir('..'+sep+'corpus-20090418')
 for each in file_list:
-    wrinting_preprocessed_file('../corpus-20090418/'+each)
+    wrinting_preprocessed_file('..'+sep+'corpus-20090418'+sep+each)
+
+for each in file_list:
+    wrinting_adv_preprocessed_file('..'+sep+'corpus-20090418'+sep+each)
